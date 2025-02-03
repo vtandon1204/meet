bb9074c23e153551ca36aceec1ecc0068824e4c4
Vaibhav Tandon <vasu.tan2017@gmail.com>
2025-02-03 22:49:28 +0530

    commit

---
+++ app/layout.tsx
@@ -0,0 +1,59 @@
+import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
+
+import "@stream-io/video-react-sdk/dist/css/styles.css";
+import 'react-datepicker/dist/react-datepicker.css';
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DiVa Meet",
+  description: "Virtual Meeting App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          // layout: {
+          //   logoImageUrl: '/icons/yoom-logo.svg',
+          //   socialButtonsVariant: 'iconButton'
+          // },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased bg-dark-2`}
+        >
+          {children}
+          <Toaster></Toaster>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="flex flex-between fixed z-40 w-full
+    bg-dark-1 px-6 py-4 lg:px-10"
+    >
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          className="max-sm: size-10"
+          alt="Yoom Logo"
+        ></Image>
+        <p className="text-[26px] font-extrabold text-white">
+          DiVa Meet
+        </p>
+      </Link>
+
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
