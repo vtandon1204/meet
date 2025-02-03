8e3863e1fc55c0b577d60f0d487aa054c31c436b
Vaibhav Tandon <vasu.tan2017@gmail.com>
2025-02-04 01:42:33 +0530

    commit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,7 +8,6 @@ const Home = () => {
     minute: "2-digit",
     timeZone: "Asia/Kolkata",
   });
-  const formattedTime = time.replace(/(am|pm)/, (match) => match.toUpperCase());
   const date = new Intl.DateTimeFormat("en-IN", {
     dateStyle: "full",
     timeZone: "Asia/Kolkata",
